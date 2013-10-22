8612a9e5bef058fd2181c1cf0576f27013e7b855
Michael Dawkins <michaeldawfr@gmail.com>
2013-10-22 23:24:20 +0200

    basic webcomponents listing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('WebPuzzleFrontApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+        $scope.webcomponents = [];
+
+        //Get data from webservice
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/web_components.json'
+        }).
+            success(function (data, status, headers, config) {
+                $scope.webcomponents = data;
+            }).
+            error(function (data, status, headers, config) {});
   });
