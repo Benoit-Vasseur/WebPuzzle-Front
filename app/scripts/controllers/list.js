1b768c2e140ed5e64fc7d77dd619c8e82d438043
Michael Dawkins <michaeldawfr@gmail.com>
2013-10-25 17:05:19 +0200

    externalized WS url

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('WebPuzzleFrontApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, WsUrl) {
         $scope.webcomponents = [];
 
         //Get data from webservice
         $http({
             method: 'GET',
-            url: 'http://webpuzzlews.herokuapp.com/web_components.json'
+            url: WsUrl + 'web_components.json'
         }).
             success(function (data, status, headers, config) {
                 $scope.webcomponents = data;
