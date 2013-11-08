b03547c285989ff76476a495ddb98d588f4ae5d0
Michael Dawkins <michaeldawfr@gmail.com>
2013-11-08 17:55:41 +0100

    conformed to conventions from jshint

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -4,36 +4,36 @@ angular.module('WebPuzzleFrontApp')
   .controller('CreateCtrl', function ($scope, $http) {
 
     $scope.wc = {
-        name :"",
-        description: "",
-        imageLink: "",
-        githubLink: "",
-        demoLink: ""
+      name: '',
+      description: '',
+      imageLink: '',
+      githubLink: '',
+      demoLink: ''
     };
 
-    $scope.oldWC = {}
+    $scope.oldWC = {};
 
-    $scope.saveWC = function(webComponent) {
-        $scope.oldWC = angular.copy(webComponent)
+    $scope.saveWC = function (webComponent) {
+      $scope.oldWC = angular.copy(webComponent);
 
-        $http({
-            method: 'POST',
-            url: 'http://webpuzzlews.herokuapp.com/web_components.json',
-            data: webComponent
+      $http({
+        method: 'POST',
+        url: 'http://webpuzzlews.herokuapp.com/web_components.json',
+        data: webComponent
+      }).
+        success(function (data, status) {
+          $scope.status = status;
+          $scope.data = data;
         }).
-            success(function(data, status) {
-                $scope.status = status;
-                $scope.data = data;
-            }).
-            error(function(data, status) {
-                $scope.data = data || "Request failed";
-                $scope.status = status;
-            });
+        error(function (data, status) {
+          $scope.data = data || 'Request failed';
+          $scope.status = status;
+        });
 
     };
 
-        $scope.isUnchanged = function(webComponent) {
-            return angular.equals(webComponent, $scope.oldWC)
-        }
+    $scope.isUnchanged = function (webComponent) {
+      return angular.equals(webComponent, $scope.oldWC);
+    };
 
   });
--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -1,70 +1,74 @@
 'use strict';
 
 angular.module('WebPuzzleFrontApp')
-    .controller('MainCtrl', function ($scope, $http, WsUrl) {
-        $scope.webcomponents = [];
+  .controller('MainCtrl', function ($scope, $http, WsUrl) {
+    $scope.webcomponents = [];
 
-        //Get data from webservice
-        $http({
-            method: 'GET',
-            url: WsUrl + 'web_components.json'
-        }).
-            success(function (data, status, headers, config) {
-                $scope.webcomponents = data;
-            }).
-            error(function (data, status, headers, config) {
-            });
-
-        $scope.filter = {
-            text: "",
-            filterObject :{
-                name : "",
-                submitter : ""
-            }
-        };
-
-        $scope.sortingTypes = [
-            {
-                "name": "ascending alphabetical order",
-                "iconCss": "fa fa-sort-alpha-asc",
-                "filter": "name"},
-            {
-                "name": "Descending alphabetical order",
-                "iconCss": "fa fa-sort-alpha-desc",
-                "filter" : "-name"},
-            {
-                "name": "Descending popularity order",
-                "iconCss": "fa fa-sort-amount-desc",
-                "filter" : "-popularity"},
-            {
-                "name": "Ascending popularity order",
-                "iconCss": "fa fa-sort-amount-asc",
-                "filter" : "popularity"}
-        ];
-
-        $scope.selectedSorting = {sortingFilterExpr : 'name'};
-
-        $scope.filterTypes = [
-            {
-                name : "By name",
-                filter : "name"
-            },
-            {
-                name : "By author",
-                filter : "submitter"
-            }
-        ];
-
-        $scope.selectedFilterType = {filter : 'name'};
-
-        $scope.$watch('filter.text', function(value){
-            $scope.filter.filterObject = {name : "", submitter : ""};
-            $scope.filter.filterObject[$scope.selectedFilterType.filter] = value;
-        });
-
-        $scope.$watch('selectedFilterType.filter', function(value){
-            $scope.filter.filterObject = {name : "", submitter : ""};
-            $scope.filter.filterObject[value] = $scope.filter.text;
-        });
-
+    //Get data from webservice
+    $http({
+      method: 'GET',
+      url: WsUrl + 'web_components.json'
+    }).
+      success(function (data) {
+        $scope.webcomponents = data;
+      }).
+      error(function () {
     });
+
+    $scope.filter = {
+      text: '',
+      filterObject: {
+        name: '',
+        submitter: ''
+      }
+    };
+
+    $scope.sortingTypes = [
+      {
+        'name': 'ascending alphabetical order',
+        'iconCss': 'fa fa-sort-alpha-asc',
+        'filter': 'name'
+      },
+      {
+        'name': 'Descending alphabetical order',
+        'iconCss': 'fa fa-sort-alpha-desc',
+        'filter': '-name'
+      },
+      {
+        'name': 'Descending popularity order',
+        'iconCss': 'fa fa-sort-amount-desc',
+        'filter': '-popularity'
+      },
+      {
+        'name': 'Ascending popularity order',
+        'iconCss': 'fa fa-sort-amount-asc',
+        'filter': 'popularity'
+      }
+    ];
+
+    $scope.selectedSorting = {sortingFilterExpr: 'name'};
+
+    $scope.filterTypes = [
+      {
+        name: 'By name',
+        filter: 'name'
+      },
+      {
+        name: 'By author',
+        filter: 'submitter'
+      }
+    ];
+
+    $scope.selectedFilterType = {filter: 'name'};
+
+    $scope.$watch('filter.text', function (value) {
+      $scope.filter.filterObject = {name: '', submitter: ''};
+      $scope.filter.filterObject[$scope.selectedFilterType.filter] = value;
+    });
+
+    $scope.$watch('selectedFilterType.filter', function (value) {
+      $scope.filter.filterObject = {name: '', submitter: ''};
+      $scope.filter.filterObject[value] = $scope.filter.text;
+    });
+
+  });
