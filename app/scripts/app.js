b4249016f6f55a76f62db179188ba878b71d8321
Michael Dawkins <michaeldawfr@gmail.com>
2013-11-13 17:21:54 +0100

    updated deps. Now using ui-router. Refactored navbar into abstract parent state.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,26 @@
 'use strict';
 
-angular.module('WebPuzzleFrontApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap.buttons', 'ui.bootstrap.tooltip', 'template/tooltip/tooltip-popup.html'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+angular.module('WebPuzzleFrontApp', ['ui.router', 'ngAnimate', 'ui.bootstrap.buttons', 'ui.bootstrap.tooltip', 'template/tooltip/tooltip-popup.html'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/app/list");
+    $urlRouterProvider.when("/", "/app/list");
+    $urlRouterProvider.otherwise("/app/list");
+
+    $stateProvider
+      .state('app', {
+        abstract: true,
+        url: '/app',
+        templateUrl: 'views/navbar.html',
+        controller: 'navBarCtrl'
+      })
+      .state('app.list',{
+        url: '/list',
         templateUrl: 'views/list.html',
         controller: 'MainCtrl'
       })
-      .when('/create', {
+      .state('app.create',{
+        url: '/create',
         templateUrl: 'views/create.html',
         controller: 'CreateCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
       });
   });
---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('WebPuzzleFrontApp')
+  .controller('navBarCtrl', function ($scope) {
+
+  });
