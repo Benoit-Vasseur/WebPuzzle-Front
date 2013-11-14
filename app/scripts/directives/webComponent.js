1796794ad6a19d31ce1d48f6ff1bdf552f12dcc7
Michael Dawkins <michaeldawfr@gmail.com>
2013-11-14 16:48:27 +0100

    [Feat] Possibility to make webcomponent titles non clickable

--- app/scripts/directives/webComponent.js
+++ app/scripts/directives/webComponent.js
@@ -4,6 +4,13 @@ angular.module('WebPuzzleFrontApp')
   .directive('webComponent', function () {
     return {
       restrict: 'E',
-      templateUrl : 'directive_templates/webComponent.html'
+      templateUrl : 'directive_templates/webComponent.html',
+      link: function(scope, element, attrs){
+        if (attrs.clickable !== undefined){
+          scope.wcClickable = attrs.clickable == 'true';
+        }else {
+          scope.wcClickable = true;
+        }
+      }
     };
   });
