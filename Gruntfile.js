33ac6288e67a3dbdaebf8b6dccf92b3f88b527c9
Michael Dawkins <michaeldawfr@gmail.com>
2013-12-06 09:33:40 +0100

    [Fix] Updated globbing pattern for dev local server

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,9 +52,9 @@ module.exports = function (grunt) {
           livereload: LIVERELOAD_PORT
         },
         files: [
-          '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/**/*.html',
           '.tmp/styles/{,*/}*.css',
-          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+          '{.tmp,<%= yeoman.app %>}/scripts/**/*.js',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
