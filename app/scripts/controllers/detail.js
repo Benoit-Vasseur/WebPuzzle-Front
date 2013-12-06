0e894e43ac02500526f12d9e9e375a7eab49ce48
Michael Dawkins <michaeldawfr@gmail.com>
2013-12-06 11:03:39 +0100

    [Feat] Jsfiddle display in detail view

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -1,19 +1,36 @@
 'use strict';
 
 angular.module('WebPuzzleFrontApp')
-  .controller('wcDetailedViewCtrl', function ($scope,$rootScope, $stateParams, $http, WsUrl) {
+  .controller('wcDetailedViewCtrl', function ($scope,$rootScope, $stateParams, $http, $sce, WsUrl) {
     $scope.wc = {
       id: $stateParams.id
     };
 
-    if(!$scope.selectedWC){}
-    $http({
-      method: 'GET',
-      url: WsUrl + 'web_components/' + $scope.wc.id + '.json'
-    }).
-      success(function (data) {
-        $rootScope.selectedWC = data;
+    function createDemoLinkUrl(){
+      $rootScope.selectedWC.demoLinkUrl = $rootScope.selectedWC.demoLink + 'embedded/result,js,html,css/';
+      $rootScope.selectedWC.demoLinkUrl = $sce.trustAsResourceUrl($rootScope.selectedWC.demoLinkUrl);
+      //@exclude
+      console.log('demoLinkUrl = ' + $rootScope.selectedWC.demoLinkUrl);
+      //@endexclude
+    }
+
+    if(!$scope.selectedWC){
+      $http({
+        method: 'GET',
+        url: WsUrl + 'web_components/' + $scope.wc.id + '.json'
       }).
-      error(function () {
-    });
+        success(function (data) {
+          $rootScope.selectedWC = data;
+          createDemoLinkUrl();
+        }).
+        error(function () {
+      });
+    } else {
+      console.log($rootScope.selectedWC);
+      if ($scope.selectedWC.demoLink){
+        if ($scope.selectedWC.demoLink.search('embedded/') === -1){
+          createDemoLinkUrl();
+        }
+      }
+    }
   });
